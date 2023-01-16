90e8d15c2040d695d4a067bc88250add5ea039ce
ANAND2023 <anandkumar2022bth@gmail.com>
2023-01-16 22:39:18 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+import ReactDOM from 'react-dom/client'
+import App from './Content/App'
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+    <>
+   <App/>
+    </>
+)
